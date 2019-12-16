39341a239428a3a6a622189ed4265eafd9b593b0
Brais <46903221+braisv@users.noreply.github.com>
2019-12-16 11:15:10 +0100

    5kyu

--- 5kyu Calculating with Functions.js
+++ 5kyu Calculating with Functions.js
@@ -19,3 +19,24 @@ eight(dividedBy(three()));
 
 // ===================== SOLUTION =======================
 
+function zero(op) { return op ? op(0) : 0 }
+function one(op) { return op ? op(1) : 1 }
+function two(op) { return op ? op(2) : 2 }
+function three(op) { return op ? op(3) : 3 }
+function four(op) { return op ? op(4) : 4 }
+function five(op) { return op ? op(5) : 5 }
+function six(op) { return op ? op(6) : 6 }
+function seven(op) { return op ? op(7) : 7 }
+function eight(op) { return op ? op(8) : 8 }
+function nine(op) { return op ? op(9) : 9 }
+
+function plus(y) { return (x) => x + y }
+function minus(y) { return (x) => x - y }
+function times(y) { return (x) => x * y }
+function dividedBy(y) { return y === 0 ? 'division by zero' : (x) => Math.floor(x / y) }
+
+seven(times(five())) // 35
+four(plus(nine())) // 13
+eight(minus(three())) // 5
+six(dividedBy(two())) // 3
+
